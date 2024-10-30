6ac72a5a9a2afd95b960d6ee8dfb0e1e3d853ed0
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-10-30 15:24:26 -0700

    Yee

--- js/quiz.js
+++ js/quiz.js
@@ -1195,7 +1195,7 @@
         card.style.opacity = 0;
         card.style.transform = 'translateY(20px)';
         setTimeout(() => {
-          card.style.transition = 'opacity 3s ease, transform 3s ease';
+          card.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
           card.style.opacity = 1;
           card.style.transform = 'translateY(0)';
         }, index * 100);
